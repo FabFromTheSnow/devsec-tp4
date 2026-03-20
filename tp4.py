66794ea9cb35dfe64ee089bc6c43b814accc0d4d
fab <fabian.mest@ynov.com>
2026-03-20 19:07:28 +0100

    Debug mode Disabled CWE-215

--- tp4.py
+++ tp4.py
@@ -285,4 +285,4 @@ if __name__ == '__main__':
     🚀 Application lancée sur http://127.0.0.1:5000
     """)
 
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=False, host='127.0.0.1', port=5000)
